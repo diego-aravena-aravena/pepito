4bb0631522ec366059374bf283c892870a592c98
diego <diegojehovaaravena@gmail.com>
2025-02-04 08:42:04 -0300

    add el contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Conster } from './componets/Conter'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
@@ -16,15 +18,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>hola + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>hola+ React</h1>
+       <Conster/>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
