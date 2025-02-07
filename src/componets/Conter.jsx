9da3b87db0c08beebb058c9e0e2ab096db4170c0
diego <diegojehovaaravena@gmail.com>
2025-02-07 14:12:56 -0300

    se ingreso Card y estilos

--- src/componets/Conter.jsx
+++ src/componets/Conter.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 
 
-export function Conster(){
-    const [count, setCount] = useState(0)
-  return(
+export function Conster() {
+  const [count, setCount] = useState(0)
+  return (
     <div className="card">
-    <button onClick={() => setCount((count) => count + 1)}>
-      count is {count}
-    </button>
-    <p>
-      Edit <code>src/App.jsx</code> and save to test 
-    </p>
-  </div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p>
+        Edit <code>src/App.jsx</code> and save to test
+      </p>
+    </div>
   )
 }
